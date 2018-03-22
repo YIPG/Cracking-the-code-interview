f5063cea90d92e013098cc7de9986cd5f2997002
YIPG <around.mr.child@gmail.com>
2018-03-23 03:43:38 +0900

    addded test

--- part1/1-3.py
+++ part1/1-3.py
@@ -1,7 +1,29 @@
-def URLify(S):
+import unittest
+
+
+def URLify(S , length):
     return S.replace(" ", "%20")
 
 
 # TODO:Test書かなあかん
-text = "google ha sugoi kamo"
-print(URLify(text))
+text = "google  ha sugoi kamo"
+L = len(text)
+print(URLify(text,L))
+
+# 3/21 テスト書いた
+class Test(unittest.TestCase):
+    data = [
+        ('overwatch love  is strong', 25,
+         'overwatch%20love%20%20is%20strong'),
+        ('kakuno mendo', 12,
+        'kakuno%20mendo')
+    ]
+
+    def test_urlify(self):
+        for [test_string, length, answer] in self.data:
+            conv = URLify(test_string, length)
+            self.assertEqual(conv, answer)
+
+
+if __name__ == "__main__":
+    unittest.main()
